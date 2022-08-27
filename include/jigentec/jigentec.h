dfc0b16a81af3bdc762b014b8e5adaeecec42756
vNaonLu <pingu326326@gmail.com>
2022-08-27 23:40:11 +0800

    adds comments to JigenTecPacket structure

--- include/jigentec/jigentec.h
+++ include/jigentec/jigentec.h
@@ -31,16 +31,36 @@ class JigenTecPacket {
   char buffer_[kMaximumPacketLength];
 
  public:
+  /**
+   * @brief Get the raw binary.
+   *
+   * @return the char binary.
+   */
   inline char *data() noexcept { return buffer_; }
 
+  /**
+   * @brief Obtain the sequence number of this packet.
+   * 
+   * @return a sequencet number and may not be consecutive.
+   */
   inline uint32_t seqence_number() noexcept {
     return ntohl(*reinterpret_cast<uint32_t *>(buffer_));
   }
 
+  /**
+   * @brief Indicate the size of payload.
+   * 
+   * @return size of payload.
+   */
   inline uint16_t payload_length() noexcept {
     return ntohs(*reinterpret_cast<uint16_t *>(buffer_ + sizeof(uint32_t)));
   }
 
+  /**
+   * @brief Get the payload binary.
+   * 
+   * @return the char binary.
+   */
   inline char *payload() noexcept {
     return reinterpret_cast<char *>(buffer_ + sizeof(uint32_t) +
                                     sizeof(uint16_t));
